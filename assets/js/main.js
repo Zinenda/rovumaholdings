33975ff91acd289a741b8e27e52ca8792fed06e5
Ivo Zinenda <izinenda@gmail.com>
2018-12-16 01:50:40 +0200

    butoes sociais

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.slider').slider();
     $('.modal').modal();
-    $('#textarea1').val('New Text');
-  M.textareaAutoResize($('#mensagem'));
+    $('#mensagem').val('New Text');
+  $('.tooltipped').tooltip();
 });
