e50a647b147d939c3be77219eab87bc9698484af
Ivo Zinenda <izinenda@gmail.com>
2018-12-16 01:04:28 +0200

    contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.slider').slider();
+    $('.modal').modal();
+    $('#textarea1').val('New Text');
+  M.textareaAutoResize($('#mensagem'));
 });
